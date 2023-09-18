f4997b98c2011d947ed626798975f8683bb6b189
mohamedtalaat47 <mohamed_talaat_47@yahoo.com>
2023-09-18 09:08:37 +0300

    init

---
+++ Data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace exportPdf
+{
+	public class Data
+	{
+		public string header { get; set; }
+		public List<string> th { get; set; }
+        public List<List<string>> rows { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using Aspose.Html;
+using Aspose.Html.Dom;
+using exportPdf;
+
+string jsonFilePath = "../../../data.json";
+string jsonString = File.ReadAllText(jsonFilePath);
+var jsonData = JsonSerializer.Deserialize<Data>(jsonString);
+
+string htmlPath = "../../../index.html";
+var document = new HTMLDocument(htmlPath);
+
+document.GetElementById("header").InnerHTML = jsonData.header;
+
+    var theadElement = document.GetElementsByTagName("thead").FirstOrDefault();
+    if (theadElement != null)
+    {
+        var trElement = theadElement.GetElementsByTagName("tr").FirstOrDefault();
+        trElement?.Remove();
+
+        var newTr = theadElement.AppendChild(document.CreateElement("tr"));
+        foreach (var h in jsonData.th)
+        {
+            var newTd = document.CreateElement("td");
+            newTd.TextContent = h;
+            newTr.AppendChild(newTd);
+        }
+    }
+
+var tbodyElement = document.GetElementsByTagName("tbody").FirstOrDefault();
+
+if (tbodyElement != null)
+{
+    var trElements = tbodyElement.GetElementsByTagName("tr").ToList();
+    foreach(var trElement in trElements)
+    {
+        trElement.Remove();
+    }
+
+    foreach (var row in jsonData.rows)
+    {
+        var newTr = tbodyElement.AppendChild(document.CreateElement("tr"));
+
+        foreach (var column in row)
+        {
+            var newTd = document.CreateElement("td");
+            newTd.TextContent = column;
+            newTr.AppendChild(newTd);
+        }
+    }
+}
+
+document.Save(htmlPath);
+
+Console.WriteLine("Done");
